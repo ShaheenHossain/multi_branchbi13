c0edaf05f837a12b21b58e959bb29f1f567656ba
Sneha Sharma <snehasharma@gmail.com>
2019-01-02 15:50:07 +0530

    Added branch module changes.

--- branch/models/inherited_res_users.py
+++ branch/models/inherited_res_users.py
@@ -6,6 +6,6 @@ from odoo import api, fields, models, _
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    branch_ids = fields.Many2many('res.branch' string="Allowed Branch")
+    branch_ids = fields.Many2many('res.branch',string="Allowed Branch")
     branch_id = fields.Many2one('res.branch', string= 'Branch')
 
