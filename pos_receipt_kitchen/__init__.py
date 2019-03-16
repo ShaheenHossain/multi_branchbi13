209a48b49a515ef7a27393e98816b766020b3a11
mark <mark@synconics.in>
2019-03-16 20:05:16 +0530

    [MOD] Added pos receipt kitchen module.

---
+++ pos_receipt_kitchen/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
