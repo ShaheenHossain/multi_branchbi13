5a87ccdd728d8551a987d6a8561ea43c53c0b403
bamuqabel <45970281+bamuqabel@users.noreply.github.com>
2019-04-16 16:47:52 +0300

    Update Branch App
    Update Branch App

--- branch/__manifest__.py
+++ branch/__manifest__.py
@@ -2,12 +2,11 @@
 
 {
     'name': 'Multiple Branch(Unit) Operation Setup for All Applications Odoo/OpenERP',
-    'version': '12.0.2.1',
+    'version': '12.0.2.2',
     'category': 'Sales',
-    'author': 'BrowseInfo',
     'summary': 'Multiple Branch/Unit Operation on Sales,Purchases,Accounting/Invoicing,Voucher,Paymemt,POS, Accounting Reports for single company',
     "description": """
-       Multiple Unit operation management for single company, Mutiple Branch management for single company, multiple operation for single company. branching company in odoo , mutliple store ,mutliple company in odoo , 
+       Multiple Unit operation management for single company, Mutiple Branch management for single company, multiple operation for single company. branching company in odoo , mutliple store ,mutliple company in odoo ,
     Branch for POS, Branch for Sales, Branch for Purchase, Branch for all, Branch for Accounting, Branch for invoicing, Branch for Payment order, Branch for point of sales, Branch for voucher, Branch for All Accounting reports, Branch Accounting filter.Branch for warehouse, branch for sale stock, branch for location
   Unit for POS, Unit for Sales, Unit for Purchase, Unit for all, Unit for Accounting, Unit for invoicing, Unit for Payment order, Unit for point of sales, Unit for voucher, Unit for All Accounting reports, Unit Accounting filter.branch unit for warehouse, branch unit for sale stock, branch unit for location
   Unit Operation for POS, Unit Operation for Sales, Unit operation for Purchase, Unit operation for all, Unit operation for Accounting, Unit Operation for invoicing, Unit operation for Payment order, Unit operation for point of sales, Unit operation for voucher, Unit operation for All Accounting reports, Unit operation Accounting filter.
@@ -15,7 +14,7 @@
        branch helpdesk and support
        branch support and helpdesk
        helpdesk branch
-       helpdesk unit 
+       helpdesk unit
        helpdek multiple unit
        helpdesk operation unit
        branch crm
@@ -40,8 +39,11 @@
 operating Unit for POS,operating Unit for Sales,operating Unit for Purchase,operating Unit for all,operating Unit for Accounting,operating Unit for invoicing,operating Unit for Payment order,operating Unit for point of sales,operating Unit for voucher,operating Unit for All Accounting reports,operating Unit Accounting filter. Operating unit for picking, operating unit for warehouse, operaing unit for sale stock, operating unit for location
 operating-Unit Operation for POS,operating-Unit Operation for Sales,operating-Unit operation for Purchase,operating-Unit operation for all, operating-Unit operation for Accounting,operating-Unit Operation for invoicing,operating-Unit operation for Payment order,operating-Unit operation for point of sales,operating-Unit operation for voucher,operating-Unit operation for All Accounting reports,operating-Unit operation Accounting filter.
     """,
+    'author': 'BrowseInfo',
     'website': 'http://www.browseinfo.in',
-    'depends': ['base','sale','purchase','stock','account','point_of_sale','purchase_stock'],
+    "price": 149.00,
+    "currency": 'EUR',
+    'depends': ['base','sale_management','purchase','stock','account','point_of_sale','purchase_stock'],
     'data': [
                 'security/branch_security.xml',
                 'security/ir.model.access.csv',
@@ -61,15 +63,13 @@ operating-Unit Operation for POS,operating-Unit Operation for Sales,operating-Un
 
              ],
     'qweb': [
-         'static/src/xml/pos.xml',
-    ],
+                'static/src/xml/pos.xml',
+            ],
     'demo': [],
-    "price": 239.00,
-    "currency": 'EUR',
     'test': [],
     'installable': True,
-    'live_test_url':'https://youtu.be/f-nqKb_jELg',
     'auto_install': False,
+    "live_test_url":'https://youtu.be/5cfsd_wkMDA',
     "images":['static/description/Banner.png'],
 }
 
