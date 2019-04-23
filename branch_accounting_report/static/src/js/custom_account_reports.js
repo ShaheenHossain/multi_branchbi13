cf4db2b3dae6f79cd033abffa80e5072c367782c
mark <mark@synconics.in>
2019-04-23 15:40:11 +0530

    [MOD] Update fin. report code.

--- branch_accounting_report/static/src/js/custom_account_reports.js
+++ branch_accounting_report/static/src/js/custom_account_reports.js
@@ -109,8 +109,8 @@ var _t = core._t;
                 if (''+el.id == ''+option_id){
                     if (el.selected === undefined || el.selected === null){el.selected = false;}
                     el.selected = !el.selected;
-                //}else if (option_value === 'ir_filters') {
-                //    el.selected = false;
+                }else if (option_value === 'ir_filters') {
+                    el.selected = false;
                 }
                 return el;
             });
@@ -172,7 +172,7 @@ var _t = core._t;
                 })
             });
 
-        partner filter
+        //partner filter
         if (this.report_options.partner) {
             if (!this.M2MFilters) {
                 var fields = {};
